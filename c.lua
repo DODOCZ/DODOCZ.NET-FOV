99f691a224eba0887166e96995e4fe2405ecf4e0
DODOCZ <dodocz.net@gmail.com>
2024-08-25 20:18:12 +0200

    Update c.lua
    Add FOV change for each vehicle category

--- c.lua
+++ c.lua
@@ -6,17 +6,44 @@ Citizen.CreateThread(function()
     local ddcz_disable = 0
     local ddcz_INPUT_AIM = 0 
 
-    
+    local vehicleCameraSettings = {
+        [0] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Compacts
+        [1] = {offset = vector3(0.0, 0.1, 0.7), fov = 95.0},  -- Sedans
+        [2] = {offset = vector3(0.0, 0.1, 0.8), fov = 90.0},  -- SUVs
+        [3] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Coupes
+        [4] = {offset = vector3(0.0, 0.1, 0.6), fov = 105.0}, -- Muscle
+        [5] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Sports Classics
+        [6] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Sports
+        [7] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Super
+        [8] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Motorcycles
+        [9] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Off-road
+        [10] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Industrial
+        [11] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Utility
+        [12] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Vans
+        [13] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Cycles
+        [14] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Boats
+        [15] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Helicopters
+        [16] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Planes
+        [17] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Service
+        [18] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Emergency
+        [19] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Military
+        [20] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Commercial
+        [21] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Trains
+        [22] = {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}, -- Open Wheel
+    }
+
     local function ddcz_clamp(value, min, max)
         return math.max(min, math.min(max, value))
     end
 
-    
     local function ddcz_setupCamera()
         local ped = PlayerPedId()
+        local vehicle = GetVehiclePedIsIn(ped, false)
+        local vehicleClass = GetVehicleClass(vehicle)
+        local camSettings = vehicleCameraSettings[vehicleClass] or {offset = vector3(0.0, 0.1, 0.6), fov = 100.0}
         local bone = GetPedBoneIndex(ped, 12844)
-        local camOffset = vector3(0.0, 0.1, 0.6)
-        local initialFov = 100.0
+        local camOffset = camSettings.offset
+        local initialFov = camSettings.fov
         local maxRotationX = 30.0
         local minRotationX = -30.0
         local maxRotationZ = 90.0
@@ -24,13 +51,11 @@ Citizen.CreateThread(function()
         local currentRotX = 0.0
         local currentRotZ = 0.0
 
-        
         ddcz_cam = CreateCam("DEFAULT_SCRIPTED_CAMERA", true)
         SetCamFov(ddcz_cam, initialFov)
         SetCamNearClip(ddcz_cam, 0.09)
         RenderScriptCams(true, false, 0, true, true) 
 
-        
         Citizen.CreateThread(function()
             while ddcz_isCameraActive do
                 Citizen.Wait(0)
@@ -39,10 +64,10 @@ Citizen.CreateThread(function()
                 local isFirstPerson = (GetFollowVehicleCamViewMode() == 4)
 
                 if isInVehicle and isFirstPerson then
-                    AttachCamToPedBone(ddcz_cam, ped, bone, 0.0, 0.1, 0.6, true)
+                    AttachCamToPedBone(ddcz_cam, ped, bone, camOffset.x, camOffset.y, camOffset.z, true)
 
                     local x = GetDisabledControlNormal(0, 1) -- X-axis mouse
-                    local y = GetDisabledControlNormal(0, 2) -- Mouse Y axis
+                    local y = GetDisabledControlNormal(0, 2) -- Y-axis mouse
 
                     -- Úprava rotace kamery podle vstupů
                     currentRotX = ddcz_clamp(currentRotX - y * 5.0, -30.0, 30.0)
@@ -53,7 +78,6 @@ Citizen.CreateThread(function()
                     local adjustedRotZ = (currentRotZ + vehicleHeading) % 360
                     SetCamRot(ddcz_cam, vector3(currentRotX, 0.0, adjustedRotZ), 2)
                 else
-                    
                     ddcz_isCameraActive = false
                     RenderScriptCams(false, false, 0, true, true)
                     DestroyCam(ddcz_cam, false)
@@ -62,13 +86,11 @@ Citizen.CreateThread(function()
         end)
     end
 
-    
     Citizen.CreateThread(function()
         while true do
             Citizen.Wait(1)
             local ped = PlayerPedId()
 
-            
             if IsControlPressed(0, ddcz_INPUT_AIM) then
                 ddcz_justpressed = ddcz_justpressed + 1
             end
@@ -93,7 +115,6 @@ Citizen.CreateThread(function()
                 ddcz_UsePerson = false
             end
 
-            
             if ddcz_isCameraActive then
                 if IsPedArmed(ped, 1) or not IsPedArmed(ped, 7) then
                     if IsControlJustPressed(0, 24) or IsControlJustPressed(0, 141) or IsControlJustPressed(0, 142) or IsControlJustPressed(0, 140) then
@@ -112,12 +133,10 @@ Citizen.CreateThread(function()
         end
     end)
 
-    -
     Citizen.CreateThread(function()
         while true do
             Citizen.Wait(1500)
             if IsPedArmed(PlayerPedId(), 6) then
-                
                 if ddcz_isCameraActive then
                     DisableControlAction(1, 140, true)
                     DisableControlAction(1, 141, true)
@@ -127,9 +146,8 @@ Citizen.CreateThread(function()
         end
     end)
 
-    
     while true do
-        Citizen.Wait(100) 
+        Citizen.Wait(100)
 
         local ped = PlayerPedId()
         local isInVehicle = IsPedInAnyVehicle(ped, false)
@@ -139,7 +157,6 @@ Citizen.CreateThread(function()
             ddcz_isCameraActive = true
             ddcz_setupCamera()
         elseif not isInVehicle and ddcz_isCameraActive then
-            
             ddcz_isCameraActive = false
             RenderScriptCams(false, false, 0, true, true)
             DestroyCam(ddcz_cam, false)
