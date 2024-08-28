cdc1f81f90531256ff061bb6afe39a0ad9f5c511
DDCZ <dodocz.net@gmail.com>
2024-08-28 19:37:30 +0200

    Update c.lua

--- c.lua
+++ c.lua
@@ -1,25 +1,25 @@
 Citizen.CreateThread(function()
-    -- Načtení konfiguračních hodnot z server.cfg
+    -- Reading configuration values from server.cfg
     local ddcz_camOffsetX = tonumber(GetConvar("profile_camOffsetX", "0.0"))
-    local ddcz_camOffsetY = tonumber(GetConvar("profile_camOffsetY", "-0.5")) -- Nastavení Y offsetu
+    local ddcz_camOffsetY = tonumber(GetConvar("profile_camOffsetY", "-0.5")) 
     local ddcz_camOffsetZ = tonumber(GetConvar("profile_camOffsetZ", "0.6"))
-    local ddcz_initialFov = tonumber(GetConvar("profile_fpsFieldOfView", "70.0")) -- Nastavení FOV (zorného úhlu)
+    local ddcz_initialFov = tonumber(GetConvar("profile_fpsFieldOfView", "70.0"))
 
-    -- Vytvoření vektoru s hodnotami offsetu kamery
+    
     local ddcz_camOffset = vector3(ddcz_camOffsetX, ddcz_camOffsetY, ddcz_camOffsetZ)
 
-    -- Proměnné pro ovládání pohledu
+    
     local ddcz_INPUT_AIM = 0
     local ddcz_Person = false
     local ddcz_justpressed = 0
     local ddcz_disable = 0
 
-    -- Hlavní vlákno pro ovládání kamery
+    
     Citizen.CreateThread(function()
         while true do
             Citizen.Wait(1)
 
-            -- Detekce stisknutí a uvolnění tlačítka pro zaměření (AIM)
+            
             if IsControlPressed(0, ddcz_INPUT_AIM) then
                 ddcz_justpressed = ddcz_justpressed + 1
             end
@@ -31,14 +31,14 @@ Citizen.CreateThread(function()
                 ddcz_justpressed = 0
             end
 
-            -- Přepnutí do pohledu z první osoby, pokud je aktuálně jiný režim
+            
             if GetFollowPedCamViewMode() == 1 or GetFollowVehicleCamViewMode() == 1 then
                 Citizen.Wait(1)
                 SetFollowPedCamViewMode(0)
                 SetFollowVehicleCamViewMode(0)
             end
 
-            -- Aktivace/deaktivace pohledu z první osoby
+            y
             if ddcz_Person then
                 if GetFollowPedCamViewMode() == 0 or GetFollowVehicleCamViewMode() == 0 then
                     Citizen.Wait(1)
@@ -52,7 +52,7 @@ Citizen.CreateThread(function()
                 ddcz_Person = false
             end
 
-            -- Deaktivace ovládacích prvků, pokud je hráč vyzbrojen a stiskne určité klávesy
+            
             local ddcz_ped = PlayerPedId()
             if IsPedArmed(ddcz_ped, 1) or not IsPedArmed(ddcz_ped, 7) then
                 if IsControlJustPressed(0, 24) or IsControlJustPressed(0, 141) or IsControlJustPressed(0, 142) or IsControlJustPressed(0, 140) then
@@ -70,7 +70,7 @@ Citizen.CreateThread(function()
         end
     end)
 
-    -- Deaktivace určitých ovládacích prvků, pokud je hráč vyzbrojen
+    
     Citizen.CreateThread(function()
         while true do
             Citizen.Wait(1)
